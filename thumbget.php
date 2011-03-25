c4a0bcf9b61495ad90b6a5073b22cc3afbef13c6
Bibhas C Debnath <iambibhas@gmail.com>
2011-03-25 15:23:59 +0530

    thumbnail generation completed

--- thumbget.php
+++ thumbget.php
@@ -1,11 +1,51 @@
 <?php
-$filename = 'pt-uploads/2sbev.jpg';
-header('Content-type: image/jpeg');
-list($width, $height) = getimagesize($filename);
-$newwidth = 200;
-$newheight = 150;
-$thumb = imagecreatetruecolor($newwidth, $newheight);
-$source = imagecreatefromjpeg($filename);
-imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-imagejpeg($thumb,NULL,90);
+$path = 'pt-uploads/dog-win.gif';
+if(file_exists($path)){
+	$size = getimagesize($path);
+	list($width, $height) = $size;
+	$mime=$size['mime'];
+	//echo $mime;
+	$newwidth = 200;
+	$newheight = 150;
+	$thumb = imagecreatetruecolor($newwidth, $newheight);
+	switch($mime){
+		case 'image/jpeg':
+			header('Content-type: image/jpeg');
+			$source = imagecreatefromjpeg($path);
+			imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+			imagejpeg($thumb,NULL,90);
+		break;
+		case 'image/png':
+			header('Content-type: image/png');
+			$source = imagecreatefrompng($path);
+			imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+			imagepng($thumb);
+		break;
+		case 'image/gif':
+			header('Content-type: image/gif');
+			$source = imagecreatefromgif($path);
+			imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+			imagegif($thumb);
+		break;
+		default:
+			header("Content-type: image/png");
+			$im = @imagecreate(200, 150)
+				or die("Cannot Initialize new GD image stream");
+			$background_color = imagecolorallocate($im, 239, 239, 239);
+			$text_color = imagecolorallocate($im, 0, 0, 0);
+			imagestring($im, 10, 60, 60,  "Invalid Format!", $text_color);
+			imagepng($im);
+			imagedestroy($im);
+		break;
+	}
+}else{
+	header("Content-type: image/png");
+	$im = @imagecreate(200, 150)
+		or die("Cannot Initialize new GD image stream");
+	$background_color = imagecolorallocate($im, 239, 239, 239);
+	$text_color = imagecolorallocate($im, 0, 0, 0);
+	imagestring($im, 10, 60, 60,  "No Image!", $text_color);
+	imagepng($im);
+	imagedestroy($im);
+}
 ?>
