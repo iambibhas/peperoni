68972c2fc619e1ce6db3b08d6b672947473a0fa6
Bibhas C Debnath <iambibhas@gmail.com>
2011-03-25 18:29:28 +0530

    full picture fetching completed

--- functions.php
+++ functions.php
@@ -3,9 +3,26 @@
  *      functions.php
  *
  */
-include 'config.php'
-function get_path_from_slug($slug){
+include 'admin/config.php';
+function get_details_from_slug($slug, $action){
 	$link=mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
 	mysql_select_db(DB_DATABASE_NAME, $link);
+	$slug=clean($slug);
+	if($action=="path")
+		$query="SELECT location FROM pt_images WHERE slug='{$slug}'";
+	elseif($action=="all")
+		$query="SELECT * FROM pt_images WHERE slug='{$slug}'";
+
+	$raw_result=mysql_query($query);
+	if(mysql_num_rows($raw_result)>0){
+		$result = mysql_fetch_assoc($raw_result);
+	}else{
+		$result=array("err"=>1);
+	}
+	return $result;
+}
+function clean($str){
+    $str = mysql_real_escape_string($str);
+    return $str;
 }
 ?>
--- picget.php
+++ picget.php
@@ -9,7 +9,11 @@ include 'functions.php';
 if(isset($_GET['slug']) && !empty($_GET['slug'])){
 	$slug=$_GET['slug'];
 
-	$path=get_path_from_slug($slug);
+	$temp=get_details_from_slug($slug,"path");
+	if(isset($temp['err']) && $temp['err']==1)
+		$path="";
+	else
+		$path=trim($temp['location']);
 
 	if(!file_exists($path)){
 		header("Content-type: image/png");
@@ -17,7 +21,7 @@ if(isset($_GET['slug']) && !empty($_GET['slug'])){
 			or die("Cannot Initialize new GD image stream");
 		$background_color = imagecolorallocate($im, 239, 239, 239);
 		$text_color = imagecolorallocate($im, 0, 0, 0);
-		imagestring($im, 10, 90, 130,  "Image file doesn't Exist", $text_color);
+		imagestring($im, 10, 90, 130,  "Image file doesn't Exist 1", $text_color);
 		imagepng($im);
 		imagedestroy($im);
 	}else{
@@ -45,7 +49,7 @@ if(isset($_GET['slug']) && !empty($_GET['slug'])){
 					or die("Cannot Initialize new GD image stream");
 				$background_color = imagecolorallocate($im, 255, 255, 255);
 				$text_color = imagecolorallocate($im, 0, 0, 0);
-				imagestring($im, 10, 50, 50,  "Image file doesn't Exist", $text_color);
+				imagestring($im, 10, 50, 50,  "Image file doesn't Exist 2", $text_color);
 				imagepng($im);
 				imagedestroy($im);
 			break;
